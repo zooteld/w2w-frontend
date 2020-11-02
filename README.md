adeba1c8cfcf86b6c5ca81734eacb35efe471975
zooteld <zooteld@gmail.com>
2020-11-01 21:56:09 -0600

    rebranding

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# w2w-frontend
- 
+# W2WPool
+W2W pool frontend for use with the w2w-nodejs-pool setup.
---
+++ config.js
@@ -0,0 +1,13 @@
+var api = "https://yourPool.address:apiPort";
+
+var email = "";
+var telegram = "";
+var discord = "";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.conceal.network/index.html#explorer";
+var transactionExplorer = "https://explorer.conceal.network/index.html#txviewer";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
